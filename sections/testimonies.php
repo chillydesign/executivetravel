093dea6075f143e97ffa4a6f2f1881b1634b917a
Charles Harvey <harvey.charles@gmail.com>
2019-01-14 08:01:27 +0000

    add css for testimonials

--- sections/testimonies.php
+++ sections/testimonies.php
@@ -1,5 +1,35 @@
+<?php $titre = get_sub_field('titre'); ?>
+<?php $testimonials = get_sub_field('testimonials'); ?>
+
 <div class="container">
-  <div class="row">
-    <div class="col-sm-8 col-sm-push-2"><?php echo get_sub_field('temoignages'); ?></div>
-  </div>
-<div>
+    <div class="row">
+        <div class="col-sm-8 col-sm-push-2">
+
+            <h2><?php echo $titre; ?></h2>
+
+            <div class="testimonials">
+
+
+                <?php foreach ($testimonials as $testimonial): ?>
+
+                    <div class="testimonial">
+                        <?php $content = $testimonial['content']; ?>
+                        <?php $client = $testimonial['client']; ?>
+
+                        <blockquote>
+                            <?php echo $content; ?>
+                        </blockquote>
+                        <cite>
+                            <?php echo $client; ?>
+                        </cite>
+
+                    </div>
+                <?php endforeach; ?>
+
+
+            </div>
+
+
+        </div>
+    </div>
+</div>
