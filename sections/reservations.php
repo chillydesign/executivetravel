98ba90fabfee4608f2cd89928e743d9686d51698
Charles Harvey <harvey.charles@gmail.com>
2019-02-07 19:08:29 +0100

    add links to reservations section

--- sections/reservations.php
+++ sections/reservations.php
@@ -10,11 +10,21 @@
 
 
 <ul class="reservation_things">
-    <li class="plane"><a href="#"><span></span><em>Vols</em></a></li>
-    <li class="sun"><a href="#"><span></span><em>Séjours</em></a></li>
-    <li class="bed"><a href="#"><span></span><em>Hotels</em></a></li>
-    <li class="house"><a href="#"><span></span><em>Appartements</em></a></li>
-    <li class="car"><a href="#"><span></span><em>Location de voiture</em></a></li>
+    <li class="plane">
+        <a target="_blank" href="https://www.e-travel.ch/onlinetravel/AMADEUS_ONLINE.htm"><span></span><em>Vols</em></a>
+    </li>
+    <li class="sun">
+        <a target="_blank" href="https://www.kendros.com/fr/web2web.aspx?W2W=3571346"><span></span><em>Séjours</em></a>
+    </li>
+    <li class="bed">
+        <a target="_blank" href="http://www.hotelpronto.com/?affiliateid=30123"><span></span><em>Hotels</em></a>
+    </li>
+    <li class="house">
+        <a target="_blank" href="https://www2.interhome.ch/fr/recherche/?iniframe=1"><span></span><em>Appartements</em></a>
+    </li>
+    <li class="car">
+        <a target="_blank" href="https://partner.sunnycars.ch/ak/452682/l/CHFR/setLang/chfr/"><span></span><em>Location de voiture</em></a>
+    </li>
 </ul>
 
 
