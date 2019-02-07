793f2351542cc209b7ac218b2a854a5182103098
Charles Harvey <harvey.charles@gmail.com>
2019-02-07 21:05:29 +0100

    added gulp watch to package

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var minify = require('gulp-minify');
+var watch = require('gulp-watch');
 //var browserSync = require('browser-sync');
 
 
@@ -13,6 +14,9 @@ gulp.task('heya', function(done) {
 
 gulp.task('sass', function(done){
   return gulp.src('scss/global.scss')
+    .pipe(watch('scss/*.scss' , function(){
+        console.log('Made CSS File');
+    } ))
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
     .pipe(autoprefixer({
         cascade: false
@@ -24,10 +28,16 @@ gulp.task('sass', function(done){
 });
 
 
+// gulp.task('watch', function () {
+//     return watch('scss/**/*.scss', { ignoreInitial: false })
+//         .pipe(
+//             gulp.dest('sass')
+//         );
+// });
 
 
 // Run all Gulp tasks and serve application
-gulp.task('default', gulp.series( 'sass', 'heya'), function() {
-  gulp.watch('scss/*.scss', ['sass']);
-//  gulp.watch('js/*.js', ['compress']);
+gulp.task('default', gulp.series( 'sass'), function() {
+  // gulp.watch('scss/*.scss', ['sass']);
+
 });
