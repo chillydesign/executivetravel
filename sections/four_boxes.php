eaefabd01f9734172d634be450fa6d9318ebde86
Charles Harvey <harvey.charles@gmail.com>
2019-02-07 19:34:52 +0100

    a

--- sections/four_boxes.php
+++ sections/four_boxes.php
@@ -15,7 +15,7 @@
                         <?php echo get_sub_field('content'); ?>
                     </div>
                 </div>
-                <?php if ($c == 2): ?>
+                <?php if ($c == 1): ?>
                 </div> <!-- END OF ROW -->
             <div class="row">
             <?php endif; ?>
