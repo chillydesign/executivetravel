e0443829111a0773b0129f807847f9bbd3587daa
Charles Harvey <harvey.charles@gmail.com>
2019-02-22 13:13:10 +0100

    add bg image to archive offree

--- archive-offre.php
+++ archive-offre.php
@@ -1,38 +1,40 @@
 <?php get_header(); ?>
 
 
-<div class="container">
+
+<?php $background_image = thumbnail_of_post_url( get_the_ID(), 'medium' );  ?>
+<h1>
+    <div class="container">
+        <?php _e( 'Offres', 'webfactor' ); ?>
+    </div>
+</h1>
+<div class="background_image" style="background-image: url(<?php echo $background_image['sizes']['large']; ?>);">
 
 
+    <div class="container">
 
-    <h1><?php _e( 'Offres', 'webfactor' ); ?></h1>
-
-
-    <section class="section  section_important_points">
-        <?php if (have_posts()): $i = 0; while (have_posts()) : the_post(); ?>
-            <?php $even_block = ($i % 2 == 0) ? 'even_bloc' : ''; ?>
-            <?php $image = thumbnail_of_post_url( get_the_ID(), 'medium' );  ?>
-            <div class="bloc  more_margin <?php echo $even_block; ?> ">
-                <div class="bloc_img" style="background-image:url('<?php echo $image; ?>');"></div>
-                <div class="bloc_text" >
-                    <h3> <?php the_title(); ?> </h3>
-                    <div><p><?php echo get_the_excerpt(); ?></p></div>
+        <section class="section  section_important_points">
+            <?php if (have_posts()): $i = 0; while (have_posts()) : the_post(); ?>
+                <?php $even_block = ($i % 2 == 0) ? 'even_bloc' : ''; ?>
+                <?php $image = thumbnail_of_post_url( get_the_ID(), 'medium' );  ?>
+                <div class="bloc  more_margin <?php echo $even_block; ?> ">
+                    <div class="bloc_img" style="background-image:url('<?php echo $image; ?>');"></div>
+                    <div class="bloc_text" >
+                        <h3> <?php the_title(); ?> </h3>
+                        <div><p><?php echo get_the_excerpt(); ?></p></div>
+                    </div>
                 </div>
-            </div>
-            <?php $i++; endwhile; endif; ?>
+                <?php $i++; endwhile; endif; ?>
+            </section>
+
+
+            <?php // get_template_part('loop'); ?>
+
+            <?php  get_template_part('pagination'); ?>
+
+
+        </div> 	<!-- end .container -->
 
 
 
-    </section>
-
-
-    <?php // get_template_part('loop'); ?>
-
-    <?php  get_template_part('pagination'); ?>
-
-
-</div> 	<!-- end .container -->
-
-
-
-<?php get_footer(); ?>
+        <?php get_footer(); ?>
