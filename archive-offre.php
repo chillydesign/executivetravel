9fc2245eae7d8a7f3d5d785f6e2b6e9dc22dcc05
Charles Harvey <harvey.charles@gmail.com>
2019-02-22 13:10:01 +0100

    more margin to bloc

--- archive-offre.php
+++ archive-offre.php
@@ -12,7 +12,7 @@
         <?php if (have_posts()): $i = 0; while (have_posts()) : the_post(); ?>
             <?php $even_block = ($i % 2 == 0) ? 'even_bloc' : ''; ?>
             <?php $image = thumbnail_of_post_url( get_the_ID(), 'medium' );  ?>
-            <div class="bloc <?php echo $even_block; ?> ">
+            <div class="bloc  more_margin <?php echo $even_block; ?> ">
                 <div class="bloc_img" style="background-image:url('<?php echo $image; ?>');"></div>
                 <div class="bloc_text" style="min-height: 500px">
                     <h3> <?php the_title(); ?> </h3>
