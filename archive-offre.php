8f589301955f0500e1af63548c0cd8fbde3efcd7
Charles Harvey <harvey.charles@gmail.com>
2019-02-22 12:30:45 +0100

    ab

--- archive-offre.php
+++ archive-offre.php
@@ -8,14 +8,14 @@
         <h1><?php _e( 'Offres', 'webfactor' ); ?></h1>
 
 
-        <section>
+        <section class="section  section_offres">
             <?php get_template_part('sections/offres'); ?>
         </section>
 
 
-        <?php get_template_part('loop'); ?>
+        <?php // get_template_part('loop'); ?>
 
-        <?php get_template_part('pagination'); ?>
+        <?php // get_template_part('pagination'); ?>
 
 
 </div> 	<!-- end .container -->
