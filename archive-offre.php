b8075bed706eea7af8fe0dafe224a27735789f20
Charles Harvey <harvey.charles@gmail.com>
2019-02-22 13:15:36 +0100

    ar

--- archive-offre.php
+++ archive-offre.php
@@ -3,17 +3,19 @@
 
 <?php $home_id = get_option( 'page_on_front' ); ?>
 <?php $background_image = thumbnail_of_post_url( $home_id, 'large' );  ?>
-<h1>
-    <div class="container">
-        <?php _e( 'Offres', 'webfactor' ); ?>
-    </div>
-</h1>
-<div class="background_image" style="background-image: url(<?php echo $background_image; ?>);">
+<section class="section  section_large_image_header">
+    <h1>
+        <div class="container">
+            <?php _e( 'Offres', 'webfactor' ); ?>
+        </div>
+    </h1>
+    <div class="background_image" style="background-image: url(<?php echo $background_image; ?>);">
+    </section>
 
 
-    <div class="container">
 
-        <section class="section  section_important_points">
+    <section class="section  section_important_points">
+        <div class="container">
             <?php if (have_posts()): $i = 0; while (have_posts()) : the_post(); ?>
                 <?php $even_block = ($i % 2 == 0) ? 'even_bloc' : ''; ?>
                 <?php $image = thumbnail_of_post_url( get_the_ID(), 'medium' );  ?>
@@ -25,14 +27,17 @@
                     </div>
                 </div>
                 <?php $i++; endwhile; endif; ?>
-            </section>
+            </div> 	<!-- end .container -->
+        </section>
 
 
+
+
+
+        <div class="container">
             <?php // get_template_part('loop'); ?>
 
             <?php  get_template_part('pagination'); ?>
-
-
         </div> 	<!-- end .container -->
 
 
