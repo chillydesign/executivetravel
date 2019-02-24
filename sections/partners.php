ce9b90e68105b7c61270cfdbd38c0e552ec45f7b
Charles Harvey <harvey.charles@gmail.com>
2019-02-24 12:56:41 +0100

    d

--- sections/partners.php
+++ sections/partners.php
@@ -13,9 +13,9 @@ $intro = get_sub_field('intro');
     <?php echo $intro; ?>
 </div>
 <ul class="slickslider_partners">
-    
+
     <?php foreach ( $partners as $post ) : setup_postdata( $post ); ?>
-        <?php $logo = get_post_thumbnail_id( get_the_ID(), 'medium' ); ?>
+        <?php $logo = get_post_thumbnail_id( get_the_ID(), 'medium' );  var_dump(get_the_ID()); var_dump($logo); ?>
         <li>
             <!-- <a href="#"> -->
             <div class="slide_image" style="background-image: url('<?php echo $logo; ?>'); background-size:contain;" ></div>
