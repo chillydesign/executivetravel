ee55edc6d5bf1fa1d4e37e4881e2a8fbd5b1d329
Charles Harvey <harvey.charles@gmail.com>
2019-03-30 10:37:14 +0100

    asdf

--- single.php
+++ single.php
@@ -1,66 +1,67 @@
 <?php get_header(); ?>
 
 
-	<!-- section -->
-	<section>
 
-	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-            <section  class="section section_large_image_header">
-                <h1>
-                    <div class="container">
-                        <?php the_title(); ?>
-                    </div>
-                </h1>
-                <?php $image = (has_post_thumbnail()) ? thumbnail_of_post_url(the_ID(), 'large') : ''; ?>
-                <div class="background_image" style="background-image: url(<?php echo $image; ?>);">
+    <!-- article -->
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+        <section  class="section section_large_image_header">
+            <h1>
+                <div class="container">
+                    <?php the_title(); ?>
                 </div>
-            </section>
+            </h1>
+            <?php $image = (has_post_thumbnail()) ? thumbnail_of_post_url(the_ID(), 'large') : ''; ?>
+            <div class="background_image" style="background-image: url(<?php echo $image; ?>);">
+            </div>
+        </section>
 
 
-            <section>
-			<div class="container">
-				<div class="row">
-					<div class="col-sm-9">
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-9">
 
-						<p class="date">Le <?php echo get_the_date(); ?></p>
+                        <p class="date">Le <?php echo get_the_date(); ?></p>
 
-						<?php the_content(); // Dynamic Content ?>
-						<?php if(get_field('attachment')) : ?>
-							<p class="attachment"><a href="<?php echo get_field('attachment')['url']; ?>" target="_blank"><?php echo get_field('nom_du_fichier_joint'); ?></a></p>
-						<?php endif; ?>
+                        <?php the_content(); // Dynamic Content ?>
+                        <?php if(get_field('attachment')) : ?>
+                            <p class="attachment"><a href="<?php echo get_field('attachment')['url']; ?>" target="_blank"><?php echo get_field('nom_du_fichier_joint'); ?></a></p>
+                        <?php endif; ?>
 
-						<?php edit_post_link(); // Always handy to leave Edit Post Links available ?>
-					</div>
-				</div>
-			</div>
-            </section>
+                        <?php edit_post_link(); // Always handy to leave Edit Post Links available ?>
+                    </div>
+                </div>
+            </div>
+        </section>
 
 
-			<?php //comments_template(); ?>
+        <?php //comments_template(); ?>
 
-		</article>
-		<!-- /article -->
+    </article>
+    <!-- /article -->
 
-	<?php endwhile; ?>
+<?php endwhile; ?>
 
-	<?php else: ?>
+<?php else: ?>
 
-		<!-- article -->
-		<article>
+    <!-- article -->
+    <article>
+        <section>
 
-			<h1><?php _e( 'Sorry, nothing to display.', 'webfactor' ); ?></h1>
 
-		</article>
-		<!-- /article -->
+            <h1><?php _e( 'Sorry, nothing to display.', 'webfactor' ); ?></h1>
 
-	<?php endif; ?>
+        </section>
+        <!-- /section -->
+    </article>
+    <!-- /article -->
 
-	</section>
-	<!-- /section -->
+<?php endif; ?>
+
 
 
 <?php //get_sidebar(); ?>
