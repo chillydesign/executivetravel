fcb8d1fc2497a121ac659fb8bfa9fd043e3a11ff
Charles Harvey <harvey.charles@gmail.com>
2019-03-30 10:34:54 +0100

    add header to single

--- single.php
+++ single.php
@@ -8,24 +8,32 @@
 
 		<!-- article -->
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+            <section  class="section section_large_image_header">
+                <h1>
+                    <div class="container">
+                        <?php the_title(); ?>
+                    </div>
+                </h1>
+                <?php $image = (has_post_thumbnail()) ? thumbnail_of_post_url(the_ID(), 'large') : ''; ?>
+                <div class="background_image" style="background-image: url(<?php echo $image; ?>);">
+                </div>
+            </section>
+
+
+
 			<div class="container">
 				<div class="row">
 					<div class="col-sm-9">
-						<h1>
-							<?php the_title(); ?>
 
-						</h1>
-
-						<!-- post details -->
 						<p class="date">Le <?php echo get_the_date(); ?></p>
-						<!-- /post details -->
 
 						<?php the_content(); // Dynamic Content ?>
 						<?php if(get_field('attachment')) : ?>
 							<p class="attachment"><a href="<?php echo get_field('attachment')['url']; ?>" target="_blank"><?php echo get_field('nom_du_fichier_joint'); ?></a></p>
 						<?php endif; ?>
 
-									<?php edit_post_link(); // Always handy to leave Edit Post Links available ?>
+						<?php edit_post_link(); // Always handy to leave Edit Post Links available ?>
 					</div>
 				</div>
 			</div>
