83a51f58d874749788ca326a27380c129cb038a6
Melissa Rissel <rissel.melissa@gmail.com>
2019-09-30 13:02:26 +0200

    oops

--- header.php
+++ header.php
@@ -36,7 +36,7 @@
             <!-- <div class="container"></div> -->
             <a href="<?php echo home_url(); ?>" class="branding"><?php bloginfo('name'); ?></a>
             <div id="menu_items">
-              <?php echo do_shortcode(['wpml_language_selector_widget'] ); ?>
+              <?php do_action('wpml_add_language_selector');  ?>
             <a href="mailto:info@e-travel.ch" id="email_button" >Email</a>
             <a href="#" id="phone_button" >Phone</a>
             <a href="#" id="menu_button" >Menu</a>
